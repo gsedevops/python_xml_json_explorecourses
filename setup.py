afcd2c1a39083d850f86ce0c3a52c6a05da018a8
Jonathan Lai <jonathanlai@gmail.com>
2024-11-14 11:00:09 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='python-xml-json-explorecourses',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/agsdot/python_xml_json_explorecourses',
+    license='MIT',
+    author='Your Name',
+    author_email='your@email.com',
+    description='Your package description'
+)
