4d0abde13627b7de4d3dcc30775a08284aaf92a3
Jonathan Lai <jonathanlai@gmail.com>
2024-12-06 11:09:01 -0800

    Make more methods publicly accessibly

--- python_xml_json_explorecourses/__init__.py
+++ python_xml_json_explorecourses/__init__.py
@@ -1,3 +1,6 @@
-from .python_xml_json_explorecourses import xml_to_dictionary
+from .python_xml_json_explorecourses import (
+    xml_to_dictionary,
+    xml_to_dictionary_exclusively_tags_search,
+)
 
-__all__ = ["xml_to_dictionary"]
+__all__ = ["xml_to_dictionary", "xml_to_dictionary_exclusively_tags_search"]
